26762df23bcc975716c0b1ae31b71d4c166e6ddc
DOA <puwadol.g@idev.co.th>
2023-08-09 20:16:38 +0700

    chore: action menu and chang page

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,5 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 pt-20">
-      <div className=''>
-        Home page 
-      </div>
-    </main>
-  )
+  return <div>Home page</div>
 }
